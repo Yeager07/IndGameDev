dd6e67fd326efebde6f156606b8a007e4d6e8874
Garek <Igor.Esipenko@urfu.me>
2026-09-23 18:42:22 +0500

    Task2
    Создал новую сцену, добавил объекты настроил вращение, изменение радиуса, изменение количества объектов

---
+++ Task1/Assets/Scripts/Task2/Rotation.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour
+{
+    private Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 rotationX = new Vector3(1.0f, 0.0f, 0.0f);
+    private Vector3 rotationY = new Vector3(0.0f, 1.0f, 0.0f);
+    private Vector3 rotationZ = new Vector3(0.0f, 0.0f, 1.0f);
+    
+    [SerializeField] private int speedRotation = 1;
+    [SerializeField] private bool rotateX = true;
+    [SerializeField] private bool rotateY = false;
+    [SerializeField] private bool rotateZ = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rotateX)
+        rotation += rotationX;
+        
+        if(rotateY)
+        rotation += rotationY;
+
+        if(rotateZ)
+        rotation += rotationZ;
+
+        transform.localRotation = Quaternion.Euler(rotation * speedRotation);
+        
+
+        /*if(rotateX)
+        {
+            transform.localRotation = Quaternion.Euler(rotationX * speedRotation);
+            rotationX += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+        
+        if(rotateY)
+        {
+            transform.localRotation = Quaternion.Euler(rotationY * speedRotation);
+            rotationY += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+
+        if(rotateZ)
+        {
+            transform.localRotation = Quaternion.Euler(rotationZ * speedRotation);
+            rotationZ += new Vector3(1.0f, 0.0f, 0.0f);
+        }*/
+    }
+}
